fc494e328e283e4cbb3a6dd82b97e10deea187a2
Shaun Hubbard <shaunhubbard2013@icloud.com>
2020-04-30 21:19:25 -0500

    swiftlint fix

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     ],
     dependencies: [
         .package(name: "FregaderoDeCocina", url: "https://github.com/pedantix/fregadero-de-cocina", from: "0.0.2"),
-        .package(name: "Nimble", url: "https://github.com/Quick/Nimble.git", from: "8.0.1"),
+        .package(name: "Nimble", url: "https://github.com/Quick/Nimble.git", from: "8.0.1")
     ],
     targets: [
         .target(
